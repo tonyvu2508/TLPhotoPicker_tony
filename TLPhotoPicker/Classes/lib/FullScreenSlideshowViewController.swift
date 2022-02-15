8952f4e05d707652872fd4457e0dbe66c131483a
Nguyen Trung Dinh <dinhtrum@gmail.com>
2022-02-15 10:18:32 +0700

    init

---
+++ TLPhotoPicker/Classes/lib/FullScreenSlideshowViewController.swift
@@ -0,0 +1,153 @@
+//
+//  FullScreenSlideshowViewController.swift
+//  ImageSlideshow
+//
+//  Created by Petr Zvoníček on 31.08.15.
+//
+
+import UIKit
+
+protocol FullScreenSlideshowViewControllerDelegate: AnyObject {
+  func didClickSubmitButton()
+}
+
+@objcMembers
+open class FullScreenSlideshowViewController: UIViewController {
+
+    open var slideshow: ImageSlideshow = {
+        let slideshow = ImageSlideshow()
+        slideshow.zoomEnabled = true
+        slideshow.contentScaleMode = UIViewContentMode.scaleAspectFit
+        slideshow.pageIndicatorPosition = PageIndicatorPosition(horizontal: .center, vertical: .bottom)
+        // turns off the timer
+        slideshow.slideshowInterval = 0
+        slideshow.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+
+        return slideshow
+    }()
+
+    /// Close button
+    open var closeButton = UIButton()
+    
+    /// Submit button
+    open var submitButton: UIButton?
+
+    /// Close button frame
+    open var closeButtonFrame: CGRect?
+
+    /// Closure called on page selection
+    open var pageSelected: ((_ page: Int) -> Void)?
+
+    /// Index of initial image
+    open var initialPage: Int = 0
+
+    /// Input sources to
+    open var inputs: [InputSource]?
+
+    /// Background color
+    open var backgroundColor = UIColor.black
+
+    /// Enables/disable zoom
+    open var zoomEnabled = true {
+        didSet {
+            slideshow.zoomEnabled = zoomEnabled
+        }
+    }
+    weak var delegate: FullScreenSlideshowViewControllerDelegate?
+    fileprivate var isInit = true
+
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+
+        self.modalPresentationStyle = .custom
+        if #available(iOS 13.0, *) {
+            // Use KVC to set the value to preserve backwards compatiblity with Xcode < 11
+            self.setValue(true, forKey: "modalInPresentation")
+        }
+    }
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = backgroundColor
+        slideshow.backgroundColor = backgroundColor
+
+        if let inputs = inputs {
+            slideshow.setImageInputs(inputs)
+        }
+
+        view.addSubview(slideshow)
+
+        // close button configuration
+        closeButton.setImage(UIImage(named: "ic_cross_white", in: .module, compatibleWith: nil), for: UIControlState())
+        closeButton.backgroundColor = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha:0.5)
+        closeButton.layer.cornerRadius = 20
+//        closeButton.layer.borderWidth = 2
+//        closeButton.layer.borderColor = UIColor.red.cgColor
+        closeButton.addTarget(self, action: #selector(FullScreenSlideshowViewController.close), for: UIControlEvents.touchUpInside)
+        view.addSubview(closeButton)
+        // submit button
+        addSubmitButton()
+    }
+    
+    private func addSubmitButton() {
+        let size = UIScreen.main.bounds.size
+        submitButton = UIButton(frame: CGRect(x: size.width - 90.0, y: size.height - 120.0, width: 70, height: 70))
+        submitButton?.setImage(UIImage(named: "100"), for: .normal)
+        submitButton?.addTarget(self, action: #selector(submitAction), for: .touchUpInside)
+        view.addSubview(submitButton!)
+    }
+    
+    @objc func submitAction(sender: UIButton!) {
+        dismiss(animated: false, completion: nil)
+        delegate?.didClickSubmitButton()
+    }
+
+    override open var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override open func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if isInit {
+            isInit = false
+            slideshow.setCurrentPage(initialPage, animated: false)
+        }
+    }
+
+    override open func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        slideshow.slideshowItems.forEach { $0.cancelPendingLoad() }
+
+        // Prevents broken dismiss transition when image is zoomed in
+        slideshow.currentSlideshowItem?.zoomOut()
+    }
+
+    open override func viewDidLayoutSubviews() {
+        if !isBeingDismissed {
+            let safeAreaInsets: UIEdgeInsets
+            if #available(iOS 11.0, *) {
+                safeAreaInsets = view.safeAreaInsets
+            } else {
+                safeAreaInsets = UIEdgeInsets.zero
+            }
+
+            closeButton.frame = closeButtonFrame ?? CGRect(x: max(10, safeAreaInsets.left), y: max(10, safeAreaInsets.top), width: 40, height: 40)
+        }
+
+        slideshow.frame = view.frame
+    }
+
+    func close() {
+        // if pageSelected closure set, send call it with current page
+        if let pageSelected = pageSelected {
+            pageSelected(slideshow.currentPage)
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
